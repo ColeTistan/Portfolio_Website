28dcd22a716306fbcd6784ee560ce6f4c8caeddc
coletistan <coletistan@gmail.com>
2021-08-13 22:54:44 -0400

    Removed component files to make static webpage

--- app.js
+++ app.js
@@ -2,20 +2,6 @@
 const menuToggle = document.getElementById('toggle-menu');
 const navList = document.getElementById('nav-list');
 
-/* import html files to app.html */
-const getHtmlFile = (fileName) => {
-    fetch(`./${fileName}/${fileName}.html`)
-        .then(response => {
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById(fileName).innerHTML = data;
-        })
-}
-
-getHtmlFile('services');
-getHtmlFile('footer');
-
 /* toggle navigation menu */
 menuToggle.addEventListener('click', () => {
     navList.classList.toggle('active');
