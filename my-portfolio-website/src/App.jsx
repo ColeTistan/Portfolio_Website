1c744f4a408d6d3c1f1517db4dd706fa35a7ff3f
coletistan <coletistan@gmail.com>
2024-08-13 14:10:16 -0400

    Added react / vite app for refactorization of portfolio webpage.

---
+++ my-portfolio-website/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import Header from "./components/header";
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ my-portfolio-website/src/components/header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">Cole Tistan</a>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Portfolio</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
